0eca2f3d3eeb4ce33b8fed0d14eb9f24fbe0b58d
CastroOlympias <blackcarrera@msn.com>
2021-05-05 16:58:49 -0600

    Have a function to take a user name and write it to a readme.md file

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const path = require('path')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your github user name?'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(data => {
+        console.log(data.userName);
+        writeToFile('read.md', generateMarkdown(data))
+    })
+}
+
+// Function call to initialize app
+init();
