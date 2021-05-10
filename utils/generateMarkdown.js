2da29f9543389484c80353a0a8e3611deaf19325
CastroOlympias <blackcarrera@msn.com>
2021-05-10 16:13:28 -0600

    using h1, h2, h3 type html elements as seen in example readmes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,31 +13,18 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-# ${data.yourName}
-# ${data.githubName}
----
+<h1 align="center"> ${data.projectName}</h1>
 
-# ${data.projectName}
-Villainous things I say
- 
-I will smite thine enemy, oh lord. Smash them beneath my feet, that they would never rise again, like the worms deep within the earth. never to rise again.
- 
-1) Diana isn't allowed to have friends
-2) I'm just here to make money, not friends
-3) It's not like I'm leading you astray
-4) Don't be a slave to your comforts
-5) Only crazy people need validation
-6) This is a little game of 'Make up the rules as you please'
-7) Just as some seek for hidden knowledge, so do others seek for ignorance.
-8) Pure imagination - I just love that song, probably because the song was written and created by occultists
-9) Don't act like you're some special victim or something, I victimized EVERYBODY
-10) You should never start a fight unless you already know you're going to win." *begins whistling
-11) I can't be the only Super Noggin around here
-12) Trust no one ><
-13) Betray every one ><
-14) You know who you are in relation to all that surrounds you, but who are you, without those things.
-15) Every word spoken and every deed performed is a seed, planted
-16) If you say nice things and I'm offended by it, then you're still an asshole
+<h2>Description<?h2>
+<h3> align="left"${data.projectDescription}</h3>
+
+<h2>Table of Contents
+- [Description](#description)
+- [Introduction](#introduction)
+- [Challenges](#challenges)
+- [Victories](#victories)
+- [Resources](#resources)
+
 
 `;
 }
